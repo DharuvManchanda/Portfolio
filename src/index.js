d9638c037b3e16ed4c9dd1f792a49b82e468feb1
DharuvManchanda <dharuvmanchanda001@gmail.com>
2024-05-05 12:15:11 +0530

    changes in packages

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
       <App />
+<SpeedInsights/>
   </BrowserRouter>
 );
 
